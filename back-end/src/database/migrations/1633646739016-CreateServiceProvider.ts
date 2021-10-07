1f0711aa11de1850d594ed719138f7d4b14319ca
Maykon Lacerda <maykonalvslacerda@gmail.com>
2021-10-07 19:48:38 -0300

    Added database connection with typeorm & migration of create service provider

---
+++ back-end/src/database/migrations/1633646739016-CreateServiceProvider.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateServiceProvider1633646739016 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
